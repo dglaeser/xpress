e16e8cd800faf1cffea07858081bdc0b72d71114
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-10-09 07:14:30 +0200

    [utils] introduce md indices and shape

--- src/adac/utils.hpp
+++ src/adac/utils.hpp
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <ostream>
+#include <utility>
 #include <type_traits>
 
 #include <cpputils/type_traits.hpp>
@@ -16,11 +18,226 @@
 
 namespace adac {
 
-//! bring in all cpputils utility
-using namespace cpputils;
-
 //! \addtogroup Utilities
 //! \{
+
+//! bring in all cpputils utilities
+using namespace cpputils;
+
+//! Instance of a compile-time index
+template<std::size_t i>
+inline constexpr index_constant<i> i_c{};
+
+
+namespace traits {
+
+template<auto a, auto b> struct is_equal : std::bool_constant<(a == b)> {};
+
+}  // namespace traits
+
+
+#ifndef DOXYGEN
+namespace detail {
+
+    template<std::size_t i, auto v>
+    struct value_i {
+        static constexpr auto at(index_constant<i>) noexcept {
+            return v;
+        }
+    };
+
+    template<typename I, auto...>
+    struct values;
+    template<std::size_t... i, auto... v> requires(sizeof...(i) == sizeof...(v))
+    struct values<std::index_sequence<i...>, v...> : value_i<i, v>... {
+        using value_i<i, v>::at...;
+    };
+
+}  // namespace detail
+#endif  // DOXYGEN
+
+//! Class to represent a list of values.
+template<auto... v>
+struct value_list : detail::values<std::make_index_sequence<sizeof...(v)>, v...> {
+    static constexpr std::size_t size = sizeof...(v);
+
+    //! Return the value at the given index in the list
+    template<std::size_t i> requires(i < size)
+    static constexpr auto at(index_constant<i> idx) {
+        using base = detail::values<std::make_index_sequence<size>, v...>;
+        return base::at(idx);
+    }
+
+    //! Perform a reduction operation on this list
+    template<typename op, typename T>
+    static constexpr auto reduce_with(op&& action, T&& initial) {
+        return _reduce_with(std::forward<op>(action), std::forward<T>(initial), v...);
+    }
+
+    //! Concatenate this list with another one
+    template<auto... _v>
+    constexpr auto operator+(const value_list<_v...>&) const {
+        return value_list<v..., _v...>{};
+    }
+
+    //! Test this list for equality with another one
+    template<auto... _v>
+    constexpr bool operator==(const value_list<_v...>&) const {
+        if constexpr (sizeof...(_v) == size)
+            return std::conjunction_v<traits::is_equal<v, _v>...>;
+        return false;
+    }
+
+    //! Write this list to the given output stream
+    friend std::ostream& operator<<(std::ostream& s, const value_list&) {
+        s << "[";
+        (s << ... << ((v == at(i_c<0>) ? "" : ", ") + std::to_string(v)));
+        s << "]";
+        return s;
+    }
+
+ private:
+    template<typename op, typename T>
+    static constexpr auto _reduce_with(op&&, T&& initial) noexcept {
+        return std::forward<T>(initial);
+    }
+
+    template<typename op, typename T, typename V0, typename... V>
+    static constexpr auto _reduce_with(op&& action, T&& initial, V0&& v0, V&&... values) noexcept {
+        auto next = action(std::forward<T>(initial), std::forward<V0>(v0));
+        if constexpr (sizeof...(V) == 0) {
+            return next;
+        } else {
+            return _reduce_with(std::forward<op>(action), std::move(next), std::forward<V>(values)...);
+        }
+    }
+};
+
+//! Type to represent a multi-dimensional shape
+template<std::size_t... s>
+struct md_shape {
+    static constexpr std::size_t size = sizeof...(s);
+    static constexpr std::size_t count = value_list<s...>{}.reduce_with(std::multiplies{}, std::size_t{1});
+
+    template<std::size_t... v> requires(size > 0)
+    constexpr bool operator==(const md_shape<v...>&) const noexcept { return false; }
+    constexpr bool operator==(const md_shape&) const noexcept { return true; }
+
+    template<std::size_t _i> requires(_i < size)
+    static constexpr std::size_t at(const index_constant<_i>& idx) noexcept {
+        return value_list<s...>::at(idx);
+    }
+};
+
+template<std::size_t... s>
+inline constexpr md_shape<s...> shape{};
+
+//! Type to represent a multi-dimensional index
+template<std::size_t... i>
+struct md_index {
+    static constexpr std::size_t size = sizeof...(i);
+
+    template<std::size_t... v> requires(size > 0)
+    constexpr bool operator==(const md_index<v...>&) const noexcept { return false; }
+    constexpr bool operator==(const md_index&) const noexcept { return true; }
+
+    template<std::size_t _i> requires(_i < size)
+    static constexpr auto at(const index_constant<_i>&) noexcept {
+        return index_constant<value_list<i...>::at(index_constant<_i>{})>{};
+    }
+
+    template<std::size_t s0, std::size_t... s> requires(sizeof...(s) == size - 1)
+    static constexpr auto as_flat_index_in(const md_shape<s0, s...>&) noexcept {
+        return i_c<_sum_up_flat_index<0>(md_shape<s...>{}, 0)>;
+    }
+
+    static constexpr auto as_flat_index_in(const md_shape<>&) noexcept requires(size == 0) {
+        return i_c<0>;
+    }
+
+ private:
+    template<std::size_t _i, std::size_t s0, std::size_t... s>
+    static constexpr std::size_t _sum_up_flat_index(const md_shape<s0, s...>& sub_shape, std::size_t current) noexcept {
+        return _sum_up_flat_index<_i + 1>(
+            md_shape<s...>{},
+            current + at(i_c<_i>).value*value_list<s0, s...>{}.reduce_with(std::multiplies{}, std::size_t{1})
+        );
+    }
+
+    template<std::size_t _i>
+    static constexpr std::size_t _sum_up_flat_index(const md_shape<>&, std::size_t current) noexcept {
+        return current + at(i_c<_i>).value;
+    }
+};
+
+template<std::size_t... i>
+inline constexpr md_index<i...> md_i_c{};
+
+
+#ifndef DOXYGEN
+namespace detail {
+
+    template<typename shape>
+    struct first_index_in;
+    template<std::size_t... s>
+    struct first_index_in<md_shape<s...>> : std::type_identity<md_index<(s*0)...>> {};
+
+}  // namespace detail
+#endif  // DOXYGEN
+
+//! Iterator over multi-dimensional indices
+template<typename shape, typename current = typename detail::first_index_in<shape>::type>
+struct md_index_iterator;
+
+template<std::size_t... s, std::size_t... i> requires(sizeof...(s) == sizeof...(i))
+struct md_index_iterator<md_shape<s...>, md_index<i...>> {
+    constexpr md_index_iterator(const md_shape<s...>&) noexcept {}
+    constexpr md_index_iterator(const md_shape<s...>&, const md_index<i...>&) noexcept {}
+
+    constexpr bool is_incrementable() const noexcept {
+        return md_index<i...>::as_flat_index_in(md_shape<s...>{}).value < md_shape<s...>::count - 1;
+    }
+
+    constexpr auto incremented() const noexcept {
+        static_assert(
+            md_index<i...>::as_flat_index_in(md_shape<s...>{}).value < md_shape<s...>::count - 1,
+            "End of the range has already been reached"
+        );
+        return adac::md_index_iterator{md_shape<s...>{}, _incremented<sizeof...(s) - 1, false>(md_index<>{})};
+    }
+
+    constexpr md_index<i...> operator*() const noexcept {
+        return {};
+    }
+
+ private:
+    template<std::size_t _i, bool was_incremented, std::size_t... c>
+    constexpr auto _incremented(md_index<c...> intermediate_result) const noexcept {
+        constexpr std::size_t at_i = md_index<i...>::at(i_c<_i>).value;
+        constexpr bool do_increment = !was_incremented && at_i < md_shape<s...>::at(i_c<_i>) - 1;
+        constexpr bool do_zero = !was_incremented && at_i >= md_shape<s...>::at(i_c<_i>) - 1;
+        constexpr auto update = [&] () constexpr {
+            if constexpr (do_increment)
+                return md_index<at_i+1, c...>{};
+            else if constexpr (do_zero)
+                return md_index<0, c...>{};
+            else
+                return md_index<at_i, c...>{};
+        } ();
+
+        if constexpr (_i > 0)
+            return _incremented<_i - 1, was_incremented || do_increment>(update);
+        else
+            return update;
+    }
+};
+
+template<typename shape>
+md_index_iterator(const shape&) -> md_index_iterator<shape, typename detail::first_index_in<shape>::type>;
+
+template<typename shape, typename index>
+md_index_iterator(const shape&, const index&) -> md_index_iterator<shape, index>;
+
 
 namespace concepts {
 
---
+++ test/test_utils.cpp
@@ -0,0 +1,117 @@
+#include <functional>
+#include <type_traits>
+#include <sstream>
+
+#include <adac/utils.hpp>
+
+#include "testing.hpp"
+
+int main() {
+    using namespace adac::testing;
+    using namespace adac;
+
+    "value_list_access"_test = [] () {
+        constexpr value_list<0, 1, 2> values;
+        static_assert(values.at(i_c<0>) == 0);
+        static_assert(values.at(i_c<1>) == 1);
+        static_assert(values.at(i_c<2>) == 2);
+    };
+
+    "value_list_equality"_test = [] () {
+        constexpr value_list<0, 1, 2> values;
+        static_assert(values == value_list<0, 1, 2>{});
+    };
+
+    "value_list_stream"_test = [] () {
+        std::ostringstream s;
+        s << value_list<0, 1, 2>{};
+        expect(eq(s.str(), std::string{"[0, 1, 2]"}));
+    };
+
+    "value_list_reduce"_test = [] () {
+        constexpr value_list<0, 1, 2> values;
+        static_assert(values.reduce_with(std::plus{}, 0) == 3);
+        static_assert(values.reduce_with(std::multiplies{}, 1) == 0);
+    };
+
+    "value_list_concat"_test = [] () {
+        constexpr value_list<0, 1, 2> values0;
+        constexpr value_list<3, 4> values1;
+        constexpr auto sum = values0 + values1;
+        static_assert(std::is_same_v<
+            std::remove_cvref_t<decltype(sum)>,
+            value_list<0, 1, 2, 3, 4>
+        >);
+    };
+
+    "md_shape_properties"_test = [] () {
+        constexpr md_shape<2, 3> shape;
+        static_assert(shape.size == 2);
+        static_assert(shape.count == 6);
+    };
+
+    "md_shape_equality"_test = [] () {
+        constexpr md_shape<2, 3> shape;
+        static_assert(shape == md_shape<2, 3>{});
+        static_assert(shape != md_shape<3, 3>{});
+    };
+
+    "md_shape_access"_test = [] () {
+        constexpr md_shape<2, 3> shape;
+        static_assert(shape.at(i_c<0>) == 2);
+        static_assert(shape.at(i_c<1>) == 3);
+    };
+
+    "md_index_equality"_test = [] () {
+        constexpr md_index<2, 3> index;
+        static_assert(index == md_index<2, 3>{});
+        static_assert(index != md_index<3, 3>{});
+    };
+
+    "md_index_access"_test = [] () {
+        constexpr md_index<2, 3> index;
+        static_assert(index.at(i_c<0>) == i_c<2>);
+        static_assert(index.at(i_c<1>) == i_c<3>);
+    };
+
+    "md_index_as_flat_index"_test = [] () {
+        static_assert(md_i_c<0, 0>.as_flat_index_in(md_shape<3, 3>{}) == i_c<0>);
+        static_assert(md_i_c<0, 1>.as_flat_index_in(md_shape<3, 3>{}) == i_c<1>);
+        static_assert(md_i_c<0, 2>.as_flat_index_in(md_shape<3, 3>{}) == i_c<2>);
+        static_assert(md_i_c<1, 0>.as_flat_index_in(md_shape<3, 3>{}) == i_c<3>);
+        static_assert(md_i_c<1, 1>.as_flat_index_in(md_shape<3, 3>{}) == i_c<4>);
+        static_assert(md_i_c<1, 2>.as_flat_index_in(md_shape<3, 3>{}) == i_c<5>);
+        static_assert(md_i_c<2, 0>.as_flat_index_in(md_shape<3, 3>{}) == i_c<6>);
+        static_assert(md_i_c<2, 1>.as_flat_index_in(md_shape<3, 3>{}) == i_c<7>);
+        static_assert(md_i_c<2, 2>.as_flat_index_in(md_shape<3, 3>{}) == i_c<8>);
+        static_assert(md_i_c<>.as_flat_index_in(md_shape<>{}) == i_c<0>);
+    };
+
+    "md_index_iterator"_test = [] () {
+        auto it = md_index_iterator{shape<2, 3>};
+        static_assert(*it == md_index<0, 0>{});
+        static_assert(it.is_incrementable());
+
+        auto inc1 = it.incremented();
+        static_assert(*inc1 == md_index<0, 1>{});
+        static_assert(inc1.is_incrementable());
+
+        auto inc2 = inc1.incremented();
+        static_assert(*inc2 == md_index<0, 2>{});
+        static_assert(inc2.is_incrementable());
+
+        auto inc3 = inc2.incremented();
+        static_assert(*inc3 == md_index<1, 0>{});
+        static_assert(inc3.is_incrementable());
+
+        auto inc4 = inc3.incremented();
+        static_assert(*inc4 == md_index<1, 1>{});
+        static_assert(inc4.is_incrementable());
+
+        auto inc5 = inc4.incremented();
+        static_assert(*inc5 == md_index<1, 2>{});
+        static_assert(!inc5.is_incrementable());
+    };
+
+    return 0;
+}
