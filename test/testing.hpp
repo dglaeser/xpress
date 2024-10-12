3bf79c2dc367a8e629c5c8ea39b2247241c6dd84
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-10-12 12:53:12 +0200

    [test] add fuzzy_eq function

--- test/testing.hpp
+++ test/testing.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cmath>
+#include <type_traits>
+
 #include <boost/ut.hpp>
 
 namespace adac::testing {
@@ -7,5 +10,10 @@ namespace adac::testing {
 using boost::ut::operator""_test;
 using boost::ut::expect;
 using boost::ut::eq;
+
+template<typename A, typename B, typename E = std::common_type_t<A, B>>
+inline constexpr bool fuzzy_eq(const A& a, const B& b, const E& eps = 1e-6) noexcept {
+    return std::abs(a - b) < eps;
+}
 
 }  // namespace adac::testing
