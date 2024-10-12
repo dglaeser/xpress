2793ab47814b45fe582d9bfe4fbb6e0e27fa53f3
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-10-12 21:38:14 +0200

    [testing] make fuzzy_eq constexpr

--- test/testing.hpp
+++ test/testing.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cmath>
 #include <type_traits>
 
 #include <boost/ut.hpp>
@@ -13,7 +12,8 @@ using boost::ut::eq;
 
 template<typename A, typename B, typename E = std::common_type_t<A, B>>
 inline constexpr bool fuzzy_eq(const A& a, const B& b, const E& eps = 1e-6) noexcept {
-    return std::abs(a - b) < eps;
+    const auto delta = a - b;
+    return (delta >= 0 && delta < eps) || (delta < 0 && delta > E{-1}*eps);
 }
 
 }  // namespace adac::testing
