3b7247550e3597c312546ed3205981601ce5d3c5
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-10-12 22:29:04 +0200

    [solvers][common] add logging helper

--- src/adac/solvers/common.hpp
+++ src/adac/solvers/common.hpp
@@ -9,6 +9,7 @@
 
 #include <utility>
 #include <type_traits>
+#include <ostream>
 
 #include <adac/bindings.hpp>
 
@@ -29,6 +30,30 @@ template<typename T = double>
 struct solver_options {
     T threshold;
     std::size_t max_iterations;
+    bool verbosity_level = 0;
+};
+
+//! Small wrapper around an std::ostream to activate/deactivate progress output
+class progress_logger {
+ public:
+    static constexpr progress_logger suppressed(std::ostream& s) noexcept { return {s, true}; }
+    static constexpr progress_logger active(std::ostream& s) noexcept { return {s, false}; }
+
+    template<typename T>
+    constexpr progress_logger& operator<<(T&& t) noexcept {
+        if (_output_active)
+            _s << std::forward<T>(t);
+        return *this;
+    }
+
+ private:
+    constexpr progress_logger(std::ostream& s, bool suppress_output) noexcept
+    : _s{s}
+    , _output_active{!suppress_output}
+    {}
+
+    std::ostream& _s;
+    bool _output_active;
 };
 
 //! \} group Solvers
